28f8db016a85639736405b29a379b435b623d530
Soumik Patra <88019138+Druky1@users.noreply.github.com>
2023-01-19 22:39:56 +0530

    Create day14.cpp

---
+++ Day 14/day14.cpp
@@ -0,0 +1,38 @@
+// Sqrt(x)
+
+class Solution {
+public:
+
+ long long int sqrtInteger(int n) {
+        
+        int start = 0;
+        int end = n;
+        long long int mid = start + (end-start)/2;
+        long long int ans = -1;
+
+        while(s<=e) {
+            
+            long long int square = mid*mid;
+            
+            if(square == n)
+                return mid;
+            
+            if(square < n ){
+                ans = mid;
+                start = mid+1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+            mid = start + (end-start)/2;
+        }
+        return ans;
+    }
+    int mySqrt(int x) {
+
+        return sqrtInteger(x);
+
+        
+    }
+};
