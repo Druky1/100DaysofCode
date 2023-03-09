37def0f5e419ded1bdad2006aca683ef27a1e633
Soumik Patra <88019138+Druky1@users.noreply.github.com>
2023-03-09 19:38:37 +0530

    Create day50.cpp

---
+++ Day 50/day50.cpp
@@ -0,0 +1,11 @@
+// Remove all Occurrences of String
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.length() != 0 && s.find(part) < s.length()){
+            s.erase(s.find(part), part.length());
+        }
+        return s;
+    }
+};
